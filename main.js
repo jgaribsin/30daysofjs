2a6b3e0c49b7e468e9b158883bbbd1a36f3b5929
jgaribsin <16392336+jgaribsin@users.noreply.github.com>
2022-02-19 01:09:41 -0700

    first day

---
+++ main.js
@@ -0,0 +1,16 @@
+let firstName = "Jonathan"
+let lastName = "Garibsingh"
+let country = "Trinidad and Tobago"
+let age = 21
+let isMarried = false
+console.log(firstName, lastName, country, age, isMarried)
+
+const gravity = 9.81 // earth gravity in m/s2
+const boilingPoint = 100 // water boiling point in Celsius
+const PI = 3.14
+console.log(gravity, boilingPoint, PI)
+
+let name = "Jonathan", 
+job = "student", 
+live = "Canada"
+console.log(name, job, live)
