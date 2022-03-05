99e9e430991fad0a4ced5ea10d80e9caa41694b1
jgaribsin <16392336+jgaribsin@users.noreply.github.com>
2022-03-04 18:34:36 -0700

    day 15 - final commit

---
+++ day15/json.js
@@ -0,0 +1,158 @@
+// JavaScript Object Notation
+// syntax is derived from JS Objects, but JSON is formatted as text only
+// lightweight data format and mostly used when data is sent server -> client
+// alternative to XML that's easier to use
+
+const example = {
+    "users": [
+        {
+            "firstName": "Jonathan",
+            "lastName": "Garibsingh",
+            "age": 21,
+            "email": "jon@jon.com"
+        },
+        {
+            "firstName": "Alex",
+            "lastName": "James",
+            "age": 25,
+            "email": "alex@alex.com"
+        },
+        {
+            "firstName": "Lidiya",
+            "lastName": "Tekle",
+            "age": 28,
+            "email": "lidiya@lidiya.com"
+        }
+    ]
+}
+
+// JSON.parse is used to change string text into a JSON format
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+
+// const usersObj = JSON.parse(usersText, undefined, 4)
+// console.log(usersObj)
+
+// usin reviver function to format data
+const usersObj = JSON.parse(usersText, (key, value) => {
+    let newValue =
+        typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+console.log(usersObj)
+
+// JSON.stringify is used to convert an object into JSON formatted data
+
+// JSON.stringify(obj, replacer, space)
+// json or text , the data
+// reviver is an optional callback function
+
+const users = {
+    Alex: {
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 20,
+        isLoggedIn: false,
+        points: 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Redux',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+        ],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+    },
+    Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Paul: {
+        email: 'paul@paul.com',
+        skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+        ],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    }
+}
+
+const txt = JSON.stringify(users, undefined, 4)
+console.log(txt) // text -> JSON as JSON is a JS Object formatted like a string
+
+// using a filter array with JSON.stringify
+// use replacer -> use keys we want to keep in an array in the place of replacer
+const user = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+}
+
+const txt = JSON.stringify(user, ['firstName', 'lastName', 'country', 'city', 'age'], 4)
+console.log(txt) // filters out everything but the keys found in the array
+
